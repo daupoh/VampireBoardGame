f57d8845e858151374d39d370413ace4ae33ce39
Стоялов Евгений <levievgen26@gmail.com>
2021-08-21 20:11:44 +0300

    Добавьте файлы проекта.

---
+++ Vampire/GameManagers/IGameMaster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VampireBoardGameTest.Vampire.GameManagers
+{
+
+    public enum GameMode
+    {
+        Four,Five,Six
+    }
+    interface IGameMaster
+    {
+
+        void SetRoles();
+
+    }
+}
